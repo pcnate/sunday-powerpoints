9729cf6a24a2f18891f32f65257955be1eff3612
Nathan Baker <pcnate@gmail.com>
2021-07-24 23:08:41 -0500

    feat: fix issue with months and allow setting month and year in CLI

--- index.js
+++ index.js
@@ -8,33 +8,63 @@ const path        = require('path')
 const moment      = require('moment')
 const to          = require('await-to-js')
 
-console.log({ argv })
-
 const SUNDAY_TEMPLATE = 'Sunday Template.pptx';
+const futureDate = moment( new Date() ).add( 7, 'day' )
 
-const currentWorkingDirectory = typeof argv.dir !== 'undefined' ? argv.dir : __dirname
+const currentWorkingDirectory = argv.dir !== null && argv.dir   !== void 0 ? argv.dir : __dirname;
+const month     = argv.month !== null && argv.month !== void 0 ? Number( argv.month ) : moment( futureDate ).month() + 1;
+const year      = argv.year  !== null && argv.year  !== void 0 ? Number( argv.year  ) : moment( futureDate ).year();
+const writeMode = argv.write !== null && argv.write !== void 0;
+
+console.log( `${ getMonthName( month ) } ${ year }` )
+console.log( `Using Directory: ${ currentWorkingDirectory }` )
+console.log( `Write mode is ${ writeMode ? 'enabled' : 'disabled, using --write to write files' }\r\n` )
 
 const templateFile = path.join( currentWorkingDirectory, SUNDAY_TEMPLATE )
 
 /**
  * get a list of sundays in the month provided
- * 
+ *
  * @param {number} m human readable month number
  * @param {number} y year
  * @returns an array of days
  */
 function sundaysInMonth( m, y ) {
   var days = new Date( y, m, 0 ).getDate();
-  var sundays = [ 8 - ( new Date( m + '/01/' + y ).getDay() ) ];
-  for ( var i = sundays[ 0 ] + 7; i < days; i += 7 ) {
-    sundays.push( i );
+  let firstSunday = null;
+  var sundays = [];
+
+  let dayOfMonth = 1
+  while( dayOfMonth <= days ) {
+    
+    firstSunday = new Date( m + '/' + dayOfMonth + '/' + y )
+
+    if ( firstSunday.getDay() === 0 ) {
+      sundays.push( dayOfMonth )
+    }
+    
+    dayOfMonth++
   }
+  
   return sundays;
 }
 
 /**
- * check if the file exists
+ * get the month name using the month number
  * 
+ * @param {number} month human readable month number
+ * @returns {string} month name
+ */
+function getMonthName( month ) {
+  const d = new Date();
+  d.setMonth( month - 1 );
+  const monthName = d.toLocaleString( "default", { month: "long" } );
+  return monthName;
+}
+
+/**
+ * check if the file exists
+ *
  * @param {string} path
  * @returns {boolean} whether or not the file was found
  */
@@ -47,10 +77,7 @@ function checkIfFileExists( path ) {
 }
 
 // lets get the date a week out so we can work on next weeks powerpoints
-let futureDate = moment( new Date() ).add( 7, 'day' )
 
-let year = moment( futureDate ).year()
-let month = moment( futureDate ).month()+1
 let sundayFiles = sundaysInMonth( month, year ).map( sunday => {
   return [
     year,
@@ -75,9 +102,13 @@ sundayFiles.map( async file => {
   let existsDone = await checkIfFileExists( filePathDone )
 
   if ( !existsTodo && !existsDone ) {
-    console.log(`Coping '${ SUNDAY_TEMPLATE }' to '${ file }'`)
-    await fs.copyFile( path.join( currentWorkingDirectory, SUNDAY_TEMPLATE ), filePathTodo )
+    if ( writeMode ) {
+      console.log(`Coping '${ SUNDAY_TEMPLATE }' to '${ file }'`)
+      await fs.copyFile( path.join( currentWorkingDirectory, SUNDAY_TEMPLATE ), filePathTodo )
+    } else {
+      console.log(`Will copy '${ SUNDAY_TEMPLATE }' to '${ file }'`)
+    }
   } else {
-    console.log(`'${ file }${ existsTodo ? ' TODO' : '' }.pptx' already exists, not overwriting`)
+    console.log(`'${ file }${ existsTodo ? ' TODO' : '' }.pptx' already exists ${ writeMode ? ', not overwriting' : '' }`)
   }
 });
